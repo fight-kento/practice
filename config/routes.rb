c41f2a42cd3feea70b9710b84e2a495421106f61
kento <nk16237053@gmail.com>
2023-04-24 22:07:38 +0900

    １回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/index'=>'posts#index'
+  get 'posts/new'=>'posts#new'
+  get'posts/:id'=> 'posts#show'
+  post'posts/create'=>'posts#create'
+  get'posts/:id/edit'=>'posts#edit'
+  post'posts/:id/update'=>'posts#update'
+  post'posts/:id/destroy'=>'posts#destroy'
+
   get '/' => 'home#top'
   get 'about' => 'home#about'
-  get'posts/index'=>'posts#index'
-
+    
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
