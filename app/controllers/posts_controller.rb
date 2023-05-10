cac47473f90eb185f32309ea3d15a55673f10abc
kento <nk16237053@gmail.com>
2023-05-10 23:27:25 +0900

    進捗報告

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = @post.user
   end
 
   def new
@@ -12,11 +16,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+      )
     if @post.save
       flash[:notice]="投稿を作成しました"
       redirect_to('/posts/index')
     else
+      puts '---------------'
+      puts @posts.errors.full_messages
+      puts '---------------'
       render("posts/new")
     end
   end
@@ -32,6 +42,9 @@ class PostsController < ApplicationController
       flash[:notice]="投稿を編集しました"
       redirect_to("/posts/index")
     else
+      puts '---------------'
+      puts @posts.errors.full_messages
+      puts '---------------'
       render("posts/edit")
     end
   end
@@ -42,5 +55,14 @@ class PostsController < ApplicationController
     flash[:notice]="投稿を削除しました"
     redirect_to('/posts/index')
   end
+
+  def ensure_correct_user
+    @post=Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
 
 end
